df858b76d2cfe9afc1999b53a3b3e2fbd042cc53
Gurgen Osipyan <osipyangurgen09@gmail.com>
2024-03-06 22:00:34 +0300

    update

---
+++ src/archive.c
@@ -0,0 +1,5 @@
+//
+// Created by gurgeno on 3/6/24.
+//
+
+#include "archive.h"
---
+++ src/archive.h
@@ -0,0 +1,8 @@
+//
+// Created by gurgeno on 3/6/24.
+//
+
+#ifndef LAB_1_ARCHIVE_H
+#define LAB_1_ARCHIVE_H
+
+#endif  // LAB_1_ARCHIVE_H
